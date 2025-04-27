242f89932bd3e825fd9b7a5c2f4c5173d348873d
Wyze69 <nathantanusubroto15@gmail.com>
2025-04-27 15:25:57 +0700

    +

---
+++ vars/mavenPipeline.groovy
@@ -0,0 +1,47 @@
+def call(){
+    pipeline {
+        agent any
+        stages {
+            stage("Hello World") {
+                steps {
+                    script {
+                        hello.hello()
+                    }
+                }
+            }
+            stage("Hello Groovy") {
+                steps {
+                    script {
+                        Output.hello(this, "Groovy")
+                    }
+                }
+            }
+            stage("Global Variable") {
+                steps {
+                    script {
+                        echo(author.name())
+                        echo(author.channel())
+                    }
+                }
+            }
+            stage("Hello Person"){
+                steps {
+                    script {
+                        hello.person([
+                            firstname: "Wyze",
+                            lastname: "root"
+                        ])
+                    }
+                }
+            }
+            stage("Library Resource"){
+                steps {
+                    script {
+                        def config = libraryResource("config/build.json")
+                        echo(config);
+                    }
+                }
+            }
+        }
+    }
+}
